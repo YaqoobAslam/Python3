5135a115a316efaa03f2599d14be550b0b36f011
DESKTOP-7JHFGB5\TASMEA <happyaqoob@gmail.com>
2017-07-22 03:16:51 +0500

    AreaOfCircle

---
+++ Chapter03/Example02.py
@@ -0,0 +1,13 @@
+'''
+Created on Jul 22, 2017
+
+@author: TASMEA
+'''
+
+if __name__ == '__main__':
+    
+  def areacircle(radius):
+        area = 3.14*radius**2
+
+        print(area)
+areacircle(5)
