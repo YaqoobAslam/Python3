b4c84a7091e51d118dff4a9abf9c8c0852e0bca0
DESKTOP-7JHFGB5\TASMEA <happyaqoob@gmail.com>
2017-07-22 03:26:54 +0500

    AreaOfTriangle

---
+++ Chapter03/Example03.py
@@ -0,0 +1,14 @@
+'''
+Created on Jul 22, 2017
+
+@author: TASMEA
+'''
+
+if __name__ == '__main__':
+    pass
+
+def aretriangle(b,h):
+    area =.5*b*h
+    print("The area of a triangle of base",b,"and height",h,"is",area)
+    
+aretriangle(5, 7)
