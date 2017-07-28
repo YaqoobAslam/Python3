3ccde70a461475e1d499b1070e9dc2a6287fb3d9
Muhammad Yaqoob <happyaqoob@gmail.com>
2017-07-28 18:16:32 +0500

    Introduction To File Handling

---
+++ Chapter07-FileHandling/Reading_From_File.py
@@ -0,0 +1,5 @@
+fhand =open('read.txt','r')
+
+for line in fhand:
+    print(line)
+
---
+++ Chapter07-FileHandling/WritingInto_File .py
@@ -0,0 +1,7 @@
+fopen =open('myfile.txt','w')
+
+line2 = 'the emblem of our land.\n'
+fopen.write(line2)
+
+fopen.close()
+
