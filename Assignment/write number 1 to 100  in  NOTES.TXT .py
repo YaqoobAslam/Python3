c72655885d95d0b50548dbc0b011c38067c75ca1
Muhammad Yaqoob <happyaqoob@gmail.com>
2017-08-02 19:24:55 +0500

    Assignment

--- Assignment/write number 1 to 100  in  NOTES.TXT .py
+++ Assignment/write number 1 to 100  in  NOTES.TXT .py
@@ -1,6 +1,6 @@
 
 
-Write a C++ program to write number 1 to 100 in a data file NOTES.TXT. solution
+Write a program to write number 1 to 100 in a data file NOTES.TXT. solution
 
 
 fopen = open('NOTES.TXT','w')
