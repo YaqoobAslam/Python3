17798ed452a6b6cc57fb9f0cfaca23a897009a34
Muhammad Yaqoob <happyaqoob@gmail.com>
2017-08-02 19:53:36 +0500

    Assignment

---
+++ Assignment/Read the content from a text file OUT.TXT.py
@@ -0,0 +1,5 @@
+Write a user-defined function in  to read the content from a text file OUT.TXT, count and display the number of alphabets present in it. solution
+
+fread = open('OUT.TXT','r')
+for line in fread:
+   print(line)
